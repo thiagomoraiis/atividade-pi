183c70f97b1bc79024e1be97e3610b1638818887
Thiago Morais Silva <morais.t@academico.ifrn.edu.br>
2023-03-31 17:06:26 -0300

    first commit

--- classes.py
+++ classes.py
@@ -1,61 +1,61 @@
 # Exercício 1 - Crie um objeto a partir da classe abaixo, chamado roc1, 
 # passando 2 parâmetros e depois faça uma chamada aos atributos e métodos
 
-# from math import sqrt
+from math import sqrt
 
-# class Rocket():
+class Rocket():
      
-#     def __init__(self, x=0, y=0):
-#         self.x = x
-#         self.y = y
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
         
-#     def move_rocket(self, x_increment=0, y_increment=1):
-#         self.x += x_increment
-#         self.y += y_increment
+    def move_rocket(self, x_increment=0, y_increment=1):
+        self.x += x_increment
+        self.y += y_increment
         
-#     def print_rocket(self):
-#         print(self.x, self.y)
+    def print_rocket(self):
+        print(self.x, self.y)
         
-# roc1 = Rocket(94, 23)
-# roc1.move_rocket()
-# roc1.print_rocket()
+roc1 = Rocket(94, 23)
+roc1.move_rocket()
+roc1.print_rocket()
 
 
 # Exercício 2 - Crie uma classe chamada Pessoa() com os atributos: nome, cidade, telefone e e-mail. Use pelo menos 2
 # métodos especiais na sua classe. Crie um objeto da sua classe e faça uma chamada a pelo menos um dos seus métodos
 # especiais.
 
-# class Pessoa():
-#     def __init__(self, nome, cidade, telefone, email):
-#         self.nome = nome
-#         self.cidade = cidade
-#         self.telefone = telefone
-#         self.email = email
+class Pessoa():
+    def __init__(self, nome, cidade, telefone, email):
+        self.nome = nome
+        self.cidade = cidade
+        self.telefone = telefone
+        self.email = email
 
-#     def falar(self):
-#         print(f'OI, meu nome é {self.nome}')
+    def falar(self):
+        print(f'OI, meu nome é {self.nome}')
 
-#     def estudar(self):
-#         print('Estou estudando')
+    def estudar(self):
+        print('Estou estudando')
 
-# pessoa = Pessoa('Thiago', 'José da Penha', '2943-4343', 'thiago@email.com')
-# pessoa.falar()
-# pessoa.estudar()
+pessoa = Pessoa('Thiago', 'José da Penha', '2943-4343', 'thiago@email.com')
+pessoa.falar()
+pessoa.estudar()
 
 
 
 # Exercício 3 - Crie a classe Smartphone com 2 atributos, tamanho e interface e crie a classe MP3Player com os 
 # atributos capacidade. A classe MP3player deve herdar os atributos da classe Smartphone.
 
-# class SmartFone():
-#     def __init__(self, tamanho, interface):
-#         self.tamanho = tamanho
-#         self.interface = interface
+class SmartFone():
+    def __init__(self, tamanho, interface):
+        self.tamanho = tamanho
+        self.interface = interface
 
     
-# class MP3player(SmartFone):
-#     def __init__(self, tamanho, interface, capacidade):
-#         super().__init__(tamanho, interface)
-#         self.capacidade = capacidade
+class MP3player(SmartFone):
+    def __init__(self, tamanho, interface, capacidade):
+        super().__init__(tamanho, interface)
+        self.capacidade = capacidade
 
 
--- funcoes.py
+++ funcoes.py
@@ -1,51 +1,51 @@
 # Exercício 1 - Crie uma função que imprima a sequência de números pares entre 1 e 20 (a função não recebe parâmetro) 
 # e depois faça uma chamada à função para listar os números 
 
-# def par():
-#     for i in range(1, 21):
-#         if i%2==0:
-#             print(i)
+def par():
+    for i in range(1, 21):
+        if i%2==0:
+            print(i)
 
-# par()
+par()
 
 
 
 # Exercício 2 - Crie uam função que receba uma string como argumento e retorne a mesma string em letras maiúsculas.
 # Faça uma chamada à função, passando como parâmetro uma string
 
-# def palavra(par):
-#     print(par.upper())
+def palavra(par):
+    print(par.upper())
 
-# var = 'hello world'
-# palavra(var)
+var = 'hello world'
+palavra(var)
 
 
 
 # Exercício 3 - Crie uma função que receba como parâmetro uma lista de 4 elementos, adicione 2 elementos a lista e 
 # imprima a lista
 
-# def func(fazoL):
-#     fazoL.append(5)
-#     fazoL.append(6)
-#     print(listinha)
+def func(fazoL):
+    fazoL.append(5)
+    fazoL.append(6)
+    print(listinha)
 
-# listinha = [1,2,3,4]
-# func(listinha)
+listinha = [1,2,3,4]
+func(listinha)
 
 
 
 # Exercício 4 - Crie uma função que receba um argumento formal e uma possível lista de elementos. Faça duas chamadas 
 # à função, com apenas 1 elemento e na segunda chamada com 4 elementos
 
-# def formal(*form):
-#     if len(form) > 1:
-#         for i in form:
-#             print(f'Lista de argumentos: {i}')
-#     else:
-#         print(f'Argumento: {form[0]}')
+def formal(*form):
+    if len(form) > 1:
+        for i in form:
+            print(f'Lista de argumentos: {i}')
+    else:
+        print(f'Argumento: {form[0]}')
 
-# formal(10)
-# formal(32, 54, 76,87)
+formal(10)
+formal(32, 54, 76,87)
 
 
 
