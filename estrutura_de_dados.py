35c332b346c92ff4552477647651fb4f6141c57f
Thiago Morais Silva <morais.t@academico.ifrn.edu.br>
2023-03-31 17:01:50 -0300

    first commit

---
+++ estrutura_de_dados.py
@@ -0,0 +1,81 @@
+# Exercício 1 - Imprima na tela os números de 1 a 10. Use uma lista para armazenar os números.
+
+# lista = []
+# for i in range(1,11):
+#     print(i)
+#     lista.append(i)
+
+# print(lista)
+
+
+
+# Exercício 2 - Crie uma lista de 5 objetos e imprima na tela
+
+# lista2 = ['cadeira', 'armario', 'mesa', 'liquidificador', 'porta']
+# for i in lista2:
+#     print(i)
+
+
+
+# Exercício 3 - Crie duas strings e concatene as duas em uma terceira string
+
+# str1 = 'Hello '
+# str2 = 'World'
+# str3 = str1 + str2
+# print(str3)
+
+# Exercício 4 - Crie uma tupla com os seguintes elementos: 1, 2, 2, 3, 4, 4, 4, 5 e depois utilize a função count do 
+# objeto tupla para verificar quantas vezes o número 4 aparece na tupla
+
+# num = (1, 2, 2, 3, 4, 4, 4, 5)
+# print(num.count(4))
+
+
+
+# Exercício 5 - Crie um dicionário vazio e imprima na tela
+
+# dicionario_vazio = {}
+# print(dicionario_vazio)
+
+
+
+# Exercício 6 - Crie um dicionário com 3 chaves e 3 valores e imprima na tela
+
+# dicionario = {'chave1': 10, 'chave2': 20, 'chave3':30}
+# for key, value in dicionario.items():
+#     print(f'Esta é a chave: {key}, e este o valor: {value}')
+
+
+
+# Exercício 7 - Adicione mais um elemento ao dicionário criado no exercício anterior e imprima na tela
+
+# dicionario['nova_chave'] = 40
+# print(dicionario)
+
+
+
+# Exercício 8 - Crie um dicionário com 3 chaves e 3 valores. 
+# Um dos valores deve ser uma lista de 2 elementos numéricos. 
+# Imprima o dicionário na tela.
+
+# novo_dic = {'chave_lista': [1,2], 'item2':50, 'item3':100}
+# print(novo_dic['chave_lista'])
+
+
+
+# Exercício 9 - Crie uma lista de 4 elementos. O primeiro elemento deve ser uma string, 
+# o segundo uma tupla de 2 elementos, o terceiro um dcionário com 2 chaves e 2 valores e 
+# o quarto elemento um valor do tipo float.
+# Imprima a lista.
+
+# lista_elementos = ['OI', (34,23), {'c1': 923, 'c2':54}, 54.42]
+# for i in lista_elementos:
+#     print(i)
+
+
+
+# Exercício 10 - Considere a string abaixo. Imprima na tela apenas os caracteres da posição 1 a 18.
+# frase = 'Cientista de Dados é o profissional mais sexy do século XXI'
+# print(frase[:18])
+
+
